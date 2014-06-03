46cde67b7ddc9691f96fa96a56ab4fd3d37e18e4
Sean Rettig <seanrettig@gmail.com>
2014-06-03 01:51:23 -0700

    done with hw5

--- hw5.pl
+++ hw5.pl
@@ -1,3 +1,6 @@
+% CS381 HW5
+% Team Members: Sean Rettig
+
 % Exercise 1
 when(275,10).
 when(261,12).
@@ -25,4 +28,24 @@ usage(P,T) :- when(C,T), where(C,P), nl.
 conflict(X,Y) :- when(X,T), when(Y,T), where(X,P), where(Y,P), X \= Y.
 
 % (d)
-meet(A,B) :- enroll(A,CA), enroll(B,CB), A \= B, where(CA,P), where(CB,P), when(CA,T1), when(CB,T2), (T1 is T2 + 1; meet(B,A)).
+meet(A,B) :- enroll(A,CA), enroll(B,CB), A \= B, where(CA,P), where(CB,P), when(CA,T1), when(CB,T2), (T1 = T2 ; (T1 is T2 + 1; meet(B,A))).
+
+% Exercise 2
+% (a)
+rdup([X|Xs],M) :- rdup(Xs,P), member(X,P), M = P.
+rdup([X|Xs],M) :- rdup(Xs,P), \+ member(X,P), append([X],P,M).
+rdup([],[]).
+
+% Answer that is technically valid but probably not intended
+rdup2(X,M) :- list_to_set(X,M).
+
+% (b)
+flat([X|Xs],F) :- is_list(X), flat(X,G), flat(Xs,H), append(G,H,F).
+flat([X|Xs],F) :- \+ is_list(X), flat(Xs,G), append([X],G,F).
+flat([],[]).
+
+% (c)
+project(P,E,L) :- project(P,E,L,0).
+project([P|Ps],[E|Es],L,C) :- D is C + 1, P = D, project(Ps,Es,M,D), append([E],M,L).
+project([P|Ps],[_|Es],L,C) :- D is C + 1, \+ P = D, project([P|Ps],Es,L,D).
+project(_,_,[],_).
